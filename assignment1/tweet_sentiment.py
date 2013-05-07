bcd965eecaf8d0a3d526e5d223b66c16d77ec619
Julian Ortega <jortegac@gmail.com>
2013-05-07 12:04:41 +0200

    Move word length calculation out of the for loop. Add some comments to the code.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -2,14 +2,17 @@ import sys
 import json
 import unicodedata
 
+#Process the sentiment input file and store them in memory as a dictionary
 def process_sentiment(sent_file):
 	sent_file_lines = sent_file.readlines()
 	sentiments = {}
 	for sentiment in sent_file_lines:
+		#Sentiment and value are separated by tab
 		words = sentiment.split("\t")
 		sentiments[words[0]] = words[1]
 	return sentiments
 	
+#Process the tweets input file and store them in memory
 def process_tweets(tweet_file):
 	tweet_file_lines = tweet_file.readlines()
 	tweets = []
@@ -28,20 +31,32 @@ def hw(sent_file, tweet_file):
 	
 	calculate_tweets_sentiment(sentiments, tweets)
 	
+#Based on the sentiment dictionary and list of tweets, calculate the sentiment of every tweet and print them
 def calculate_tweets_sentiment(sentiments, tweets):
+	#Iterate through all the tweets
 	for tweet in tweets:
 		words = tweet.split()
+		
+		#The sentiment score
 		score = float(0)
-		num = float(0)
-		for word in words:
-			num = len(words)
+		
+		#Number of words in the tweet
+		num = len(words)
+		
+		#Iterate through every word in the tweet
+		for word in words:			
 			try:	
+				#Find the word, get its score and add it to the current tweet score
 				score += float(sentiments[word])
 			except (NameError, TypeError, KeyError):
+				#If the word is not in the dictionary, ignore the error and continue
 				pass
+		
+		#No word in the tweet was found in the sentiment dictionary
 		if num == 0:
 			print "0"
 		else:
+			#Average the score by the number of words in the tweet
 			print score/num
 		
 		
@@ -53,8 +68,6 @@ def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     hw(sent_file, tweet_file)
-    #lines(sent_file)
-    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
